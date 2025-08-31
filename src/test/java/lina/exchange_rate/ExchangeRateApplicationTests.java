b4ab3e9ed5802eff3d338a9be7221925f3e2ec32
Detelina Valerieva <detelinavalerieva@Detelinas-MacBook-Air.local>
2025-08-31 12:07:13 +0200

    Initial commit - Spring Boot skeleton

---
+++ src/test/java/lina/exchange_rate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package lina.exchange_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
